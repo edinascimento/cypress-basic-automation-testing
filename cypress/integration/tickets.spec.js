a786a495bc487dac1676bfcbe375a3ef137b095f
Édipo Nascimento <esnedipo@gmail.com>
2022-02-17 17:45:22 -0300

    update gitignore

---
+++ cypress/integration/tickets.spec.js
@@ -0,0 +1,11 @@
+const baseURL = "https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"
+
+beforeEach(() => {
+  cy.visit(baseURL)
+})
+
+describe('', () => {
+  it('', () => {
+    
+  });
+});
