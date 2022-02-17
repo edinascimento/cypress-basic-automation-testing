ffe8a9915632383f82a4995e3ebfd46368a3f205
Édipo Nascimento <esnedipo@gmail.com>
2022-02-17 17:53:03 -0300

    browsing througg url

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,11 +1,10 @@
 const baseURL = "https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"
 
-beforeEach(() => {
-  cy.visit(baseURL)
-})
+describe("Tickets", () => {
 
-describe('', () => {
-  it('', () => {
+  beforeEach(() => cy.visit(baseURL))
+
+  it("has 'TICKETBOX' header's ", () => {
     
   });
 });
