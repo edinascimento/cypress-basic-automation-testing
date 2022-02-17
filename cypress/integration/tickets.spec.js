a203d3084550ad80909a5f4f67795032e789c569
Édipo Nascimento <esnedipo@gmail.com>
2022-02-17 18:15:11 -0300

    fill in input text

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -5,6 +5,14 @@ describe("Tickets", () => {
   beforeEach(() => cy.visit(baseURL))
 
   it("has 'TICKETBOX' header's ", () => {
-    
+
+    const firstname = "Edipo";
+    const lastname = "Nascimento";
+
+    cy.get("#first-name").type(firstname);
+    cy.get("#last-name").type(lastname);
+    cy.get("#email").type("esntest@gmail.com");
+    cy.get("#requests").type("Vegetarian")
+    cy.get("#signature").type(`${firstname} ${lastname}`) 
   });
 });
